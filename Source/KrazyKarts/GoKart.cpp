0ba21aa4666d8bfa7d488f4b75ca134741688194
halxp <alexandre.pelletier@gmail.com>
2022-01-20 18:10:43 +0100

    2. Creating A Go-Kart Pawn

---
+++ Source/KrazyKarts/GoKart.cpp
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+#include "GoKart.h"
+
+#include "Components/InputComponent.h"
+
+// Sets default values
+AGoKart::AGoKart()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AGoKart::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AGoKart::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	FVector DeltaLocation = Velocity * DeltaTime * 100.f;
+	AddActorWorldOffset(DeltaLocation);
+}
+
+// Called to bind functionality to input
+void AGoKart::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis("MoveForward", this, &AGoKart::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &AGoKart::MoveRight);
+}
+
+
+void AGoKart::MoveForward(float Val)
+{
+	Velocity = GetActorForwardVector() * 20.f * Val;
+}
+
+void AGoKart::MoveRight(float Val)
+{
+	//GetVehicleMovementComponent()->SetSteeringInput(Val);
+}
